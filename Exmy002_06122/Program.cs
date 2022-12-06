8e9faa1239ed1265e2d5309cf87c38d70606d91a
«Alena» <a9214094331@gmail.com>
2022-12-06 22:11:10 +0300

    commit3  problem solving 002 06122022

---
+++ Exmy002_06122/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Введите число:");
+int limit = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {limit} равно {PR(limit)}");
+
+int PR (int limit)
+{
+    int PR = 1;
+    for (int i = 1; i<= limit; i++)
+        PR *= i;
+        return PR;
+}
+
+//работает!
