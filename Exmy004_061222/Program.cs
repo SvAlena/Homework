341c07323c03e8047fcd2142fd72561f837978da
«Alena» <a9214094331@gmail.com>
2022-12-06 22:08:50 +0300

    commit1 problem solving 06122022

---
+++ Exmy004_061222/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[i])
+            {
+                int temporary = array[i];
+                array[i] = array[j];
+                array[j] = temporary;
+            }
+        }
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
+
