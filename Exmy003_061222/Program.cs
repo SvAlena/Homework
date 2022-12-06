4717bc91238fe34ca945d4fdddb30da58bcc731d
«Alena» <a9214094331@gmail.com>
2022-12-06 22:10:40 +0300

    commit2  problem solving 003 06122022

---
+++ Exmy003_061222/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+ */
+
+int[] array = new int[8];
+var random = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 2);
+}
+Console.WriteLine(string.Join(", ", array));
+
+//получилось!
